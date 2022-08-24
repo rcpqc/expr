cafe17957a2eaeab4211a6eeb1525f61974aa28a
rcpqc <1015272901@qq.com>
2022-08-24 14:16:47 +0800

    feat: timefmt -> tfmt

--- builtin/time.go
+++ builtin/time.go
@@ -3,9 +3,9 @@ package builtin
 import "time"
 
 func init() {
-	Functions["timefmt"] = timefmt
+	Functions["tfmt"] = tfmt
 }
 
-func timefmt(timestamp int64, layout string) string {
+func tfmt(timestamp int64, layout string) string {
 	return time.Unix(timestamp, 0).Format(layout)
 }
