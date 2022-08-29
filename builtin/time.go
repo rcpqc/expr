ab7952ddc30a33fd8873f036dfac769dc35be613
rcpqc <1015272901@qq.com>
2022-08-29 23:43:33 +0800

    feat: 增加时间函数

--- builtin/time.go
+++ builtin/time.go
@@ -3,9 +3,25 @@ package builtin
 import "time"
 
 func init() {
+	Functions["time"] = _time
 	Functions["tfmt"] = tfmt
+	Functions["tenc"] = tenc
+	Functions["tdec"] = tdec
+}
+
+func _time(timestamp int64) time.Time {
+	return time.Unix(timestamp, 0)
 }
 
 func tfmt(timestamp int64, layout string) string {
 	return time.Unix(timestamp, 0).Format(layout)
+}
+
+func tenc(t time.Time, layout string) string {
+	return t.Format(layout)
+}
+
+func tdec(s string, layout string) time.Time {
+	t, _ := time.Parse(layout, s)
+	return t
 }
