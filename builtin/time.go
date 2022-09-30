140bdf012d87758d74918fe0dcf0ba4ccb01ce41
rcpqc <1015272901@qq.com>
2022-09-30 17:07:21 +0800

    feat(time): 精简函数

--- builtin/time.go
+++ builtin/time.go
@@ -5,21 +5,15 @@ import "time"
 func init() {
 	Functions["time"] = _time
 	Functions["tnow"] = tnow
-	Functions["tunix"] = tunix
 	Functions["tfmt"] = tfmt
 	Functions["tprs"] = tprs
-	Functions["tenc"] = tenc
-	Functions["tdec"] = tdec
 }
 
 func _time(timestamp int64) time.Time {
 	return time.Unix(timestamp, 0)
 }
-func tnow() time.Time {
-	return time.Now()
-}
-func tunix(t time.Time) int64 {
-	return t.Unix()
+func tnow() int64 {
+	return time.Now().Unix()
 }
 func tfmt(timestamp int64, layout string) string {
 	return time.Unix(timestamp, 0).Format(layout)
@@ -27,11 +21,4 @@ func tfmt(timestamp int64, layout string) string {
 func tprs(s string, layout string) int64 {
 	t, _ := time.Parse(layout, s)
 	return t.Unix()
-}
-func tenc(t time.Time, layout string) string {
-	return t.Format(layout)
-}
-func tdec(s string, layout string) time.Time {
-	t, _ := time.Parse(layout, s)
-	return t
 }
