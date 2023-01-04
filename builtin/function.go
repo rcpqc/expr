d132da55f7813b83c3b068cf09e86237163e3ecc
rcpqc <1015272901@qq.com>
2023-01-05 00:49:25 +0800

    feat: 新增len & cap

--- builtin/function.go
+++ builtin/function.go
@@ -1,3 +1,10 @@
 package builtin
 
+import "reflect"
+
 var Functions = map[string]interface{}{}
+
+func init() {
+	Functions["len"] = func(v interface{}) int { return reflect.ValueOf(v).Len() }
+	Functions["cap"] = func(v interface{}) int { return reflect.ValueOf(v).Cap() }
+}
