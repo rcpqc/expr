957d055f77b1879d508561e72da32d88c3449306
rcpqc <1015272901@qq.com>
2023-04-01 04:08:46 +0800

    feat(index): 支持map

--- eval/index.go
+++ eval/index.go
@@ -4,26 +4,53 @@ import (
 	"fmt"
 	"go/ast"
 	"reflect"
+
+	"github.com/rcpqc/expr/types"
 )
+
+func evalIndexArray(rvx reflect.Value, i interface{}) (interface{}, error) {
+	ri := reflect.ValueOf(i)
+	if !ri.IsValid() || !ri.CanConvert(types.IntType) {
+		return nil, fmt.Errorf("[index] index(%v) can't convert to int", ri.Type())
+	}
+	idx := int(ri.Int())
+	if idx < 0 {
+		idx += rvx.Len()
+	}
+	if idx < 0 || idx > rvx.Len() {
+		return nil, fmt.Errorf("[index] out of range index(%d) for len(%d)", idx, rvx.Len())
+	}
+	return rvx.Index(idx).Interface(), nil
+}
+
+func evalIndexMap(rvx reflect.Value, key interface{}) (interface{}, error) {
+	rkey := reflect.ValueOf(key)
+	if rvx.Type().Key() != rkey.Type() {
+		return nil, fmt.Errorf("[index] %v can't index by key(%v)", rvx.Type(), rkey.Type())
+	}
+	val := rvx.MapIndex(reflect.ValueOf(key))
+	if !val.IsValid() || !val.CanInterface() {
+		return reflect.Zero(rvx.Type().Elem()).Interface(), nil
+	}
+	return val.Interface(), nil
+}
 
 func evalIndex(index *ast.IndexExpr, variables Variables) (interface{}, error) {
 	x, err := eval(index.X, variables)
 	if err != nil {
 		return nil, err
 	}
+	idx, err := eval(index.Index, variables)
+	if err != nil {
+		return nil, err
+	}
 	rvx := reflect.ValueOf(x)
-	if rvx.Kind() != reflect.Slice && rvx.Kind() != reflect.Array && rvx.Kind() != reflect.String {
+	switch rvx.Kind() {
+	case reflect.Slice, reflect.Array, reflect.String:
+		return evalIndexArray(rvx, idx)
+	case reflect.Map:
+		return evalIndexMap(rvx, idx)
+	default:
 		return nil, fmt.Errorf("[index] illegal kind(%s)", rvx.Kind())
 	}
-	idx, err := evalInt(index.Index, variables)
-	if err != nil {
-		return nil, fmt.Errorf("[index] index err: %v", err)
-	}
-	if idx < 0 {
-		idx += rvx.Len()
-	}
-	if idx < 0 || idx > rvx.Len() {
-		return 0, fmt.Errorf("[index] out of range index(%d) for len(%d)", idx, rvx.Len())
-	}
-	return rvx.Index(idx).Interface(), nil
 }
